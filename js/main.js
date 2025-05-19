072eabad9e13482a40926e827a169e24fc41264a
DonovanFranco <161343179+DonovanFranco@users.noreply.github.com>
2025-05-19 11:27:29 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@
     };
 
     // Hacemos el POST usando fetch
-    fetch('http://18.215.184.8/api/devices', {
+    fetch('http://3.239.158.119/api/devices', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
